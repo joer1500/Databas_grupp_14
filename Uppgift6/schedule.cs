e1f15f05c71e54024b79394baddecf663de25708
ebbawedin <51076044+ebbawedin@users.noreply.github.com>
2019-05-27 12:40:27 +0200

    lagt till classen schedule med tillhörande properties

---
+++ Uppgift6/schedule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift6
+{
+    class Schedule
+    {
+        public int id { get; set; }
+        public int schoolchild_id { get; set; }
+        public DateTime date { get; set; }
+        public string day_off { get; set; }
+        public string breakfast { get; set; }
+        public DateTime should_drop { get; set; }
+        public DateTime should_pickup { get; set; }
+        public string walk_home_alone { get; set; }
+        public string home_with_friend { get; set; }
+
+    }
+}
