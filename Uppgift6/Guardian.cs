2c5c5d35e458194160cc8b49995a1a115c886b55
ebbawedin <51076044+ebbawedin@users.noreply.github.com>
2019-05-28 10:43:38 +0200

    Lagt till GetAllGuardian metod i dbOp

---
+++ Uppgift6/Guardian.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift6
+{
+    class Guardian
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string phonenumber { get; set; }
+        public string  address { get; set; }
+
+    }
+}
