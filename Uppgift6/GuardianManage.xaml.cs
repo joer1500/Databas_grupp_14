caa1dd16b2306c59fe2b021385db6cdc8817fb11
joer1500 <51077409+joer1500@users.noreply.github.com>
2019-05-29 10:58:32 +0200

    Lagt till funktioner

---
+++ Uppgift6/GuardianManage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Uppgift6
+{
+    /// <summary>
+    /// Interaction logic for GuardianManage.xaml
+    /// </summary>
+    public partial class GuardianManage : Window
+    {
+        public GuardianManage()
+        {
+            InitializeComponent();
+            UpdateListView();
+        }
+
+        Guardian selectedGuardian;
+
+        private void UpdateListView()
+        {
+            DbOperations db = new DbOperations();
+
+            listViewGuardians.ItemsSource = null;
+            listViewGuardians.ItemsSource = db.GetAllGuardians();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            StaffChildren s = new StaffChildren();
+            s.Show();
+            this.Close();
+        }
+
+        private void listViewGuardians_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedGuardian = (Guardian)listViewGuardians.SelectedItem;
+        }
+
+        private void listViewGuardians_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            selectedGuardian = (Guardian)listViewGuardians.SelectedItem;
+            if (selectedGuardian == null)
+            {
+                return;
+            }
+            else
+            {
+                MessageBox.Show($"Förnamn: {selectedGuardian.firstname}\rEfternamn: {selectedGuardian.lastname}\rTelefonnummer: {selectedGuardian.phonenumber}\rAdress: {selectedGuardian.address}\r", "Information om vårdnadshavare");
+            }
+        }
+    }
+}
